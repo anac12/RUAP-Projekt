826015be3a80ef29123710f7648537f6928f44e6
unknown <cacijaana1@gmail.com>
2019-10-25 17:43:20 +0200

    My name added to hello

---
+++ ConsoleApplication7.cpp
@@ -0,0 +1,16 @@
+// ConsoleApplication7.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+
+
+int main(){
+
+	cout << "Hello\n";
+
+	cout << "Pozdrav iz aplikacije od Ane Cacije\n";
+	return 0;
+}
